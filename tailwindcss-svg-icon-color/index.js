c5b59d2b700d00e3bd47b4c415912a48dc59a298
Kevin Lobine <i@kevinlobine.com>
2023-11-01 14:23:03 +0100

    feat: add basic docs

--- tailwindcss-svg-icon-color/index.js
+++ tailwindcss-svg-icon-color/index.js
@@ -1,19 +1,19 @@
 const plugin = require("tailwindcss/plugin");
-
 const getFlattenedObject = require("./src/getFlattenedObject");
 
 module.exports = plugin.withOptions(function (options = {}) {
   return function ({ matchUtilities, theme }) {
-    const className = options.className ?? 'icon-color'
-    const colors = theme('colors');
+    const className = options.className ?? "icon-color";
+    const colors = theme("colors");
 
     const flattenedColors = getFlattenedObject(colors);
 
     matchUtilities(
       {
-        [`${className}`]: value => ({
-          '> path': {
+        [`${className}`]: (value) => ({
+          "> path": {
             fill: value,
+            stroke: value,
           },
         }),
       },
